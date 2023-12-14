9cef50b22d4e9f031741290c1c8bffda4d258ab6
gemmaro <gemmaro.dev@gmail.com>
2023-12-14 22:49:02 +0900

    [ style ] Guix manifest

--- manifest.scm
+++ manifest.scm
@@ -4,22 +4,25 @@
              (guix packages)
              (guix download)
              (guix build-system ruby)
-             ((guix licenses) #:prefix license:)
+             ((guix licenses)
+              #:prefix license:)
              (gnu packages perl)
              (gemmaro packages perl))
 
-(setenv "PERL5LIB" (string-append (getenv "PWD") "/_translation/po4a/lib:" (getenv "PERL5LIB")))
+(setenv "PERL5LIB"
+        (string-append (getenv "PWD") "/_translation/po4a/lib:"
+                       (getenv "PERL5LIB")))
 
 (define-public ruby-guard
   (package
     (name "ruby-guard")
     (version "2.18.0")
-    (source (origin
-              (method url-fetch)
-              (uri (rubygems-uri "guard" version))
-              (sha256
-               (base32
-                "1zqy994fr0pf3pda0x3mmkhgnfg4hd12qp5bh1s1xm68l00viwhj"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (rubygems-uri "guard" version))
+       (sha256
+        (base32 "1zqy994fr0pf3pda0x3mmkhgnfg4hd12qp5bh1s1xm68l00viwhj"))))
     (build-system ruby-build-system)
     (arguments
      `(#:tests? #f)) ;TODO
@@ -43,12 +46,12 @@ modifications.")
   (package
     (name "ruby-guard-compat")
     (version "1.2.1")
-    (source (origin
-              (method url-fetch)
-              (uri (rubygems-uri "guard-compat" version))
-              (sha256
-               (base32
-                "1zj6sr1k8w59mmi27rsii0v8xyy2rnsi09nqvwpgj1q10yq1mlis"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (rubygems-uri "guard-compat" version))
+       (sha256
+        (base32 "1zj6sr1k8w59mmi27rsii0v8xyy2rnsi09nqvwpgj1q10yq1mlis"))))
     (build-system ruby-build-system)
     (arguments
      `(#:tests? #f)) ;TODO
@@ -61,12 +64,12 @@ modifications.")
   (package
     (name "ruby-guard-shell")
     (version "0.7.2")
-    (source (origin
-              (method url-fetch)
-              (uri (rubygems-uri "guard-shell" version))
-              (sha256
-               (base32
-                "0va3brhgc0lsvayjizyhzdybikzlyx9kfqpdgdglclbwb5611kf9"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (rubygems-uri "guard-shell" version))
+       (sha256
+        (base32 "0va3brhgc0lsvayjizyhzdybikzlyx9kfqpdgdglclbwb5611kf9"))))
     (build-system ruby-build-system)
     (arguments
      `(#:tests? #f)) ;TODO
@@ -80,4 +83,13 @@ modifications.")
     (home-page "http://github.com/sdwolfz/guard-shell")
     (license license:expat)))
 
-(packages->manifest (list po4a node-lts jekyll ruby-guard ruby-guard-shell ruby-rubocop ruby tidyall perl-perl-tidy perl))
+(packages->manifest (list po4a
+                          node-lts
+                          jekyll
+                          ruby-guard
+                          ruby-guard-shell
+                          ruby-rubocop
+                          ruby
+                          tidyall
+                          perl-perl-tidy
+                          perl))
